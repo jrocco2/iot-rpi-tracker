e8a8849394758466c153639461e714dfe8fe4946
Joseph <josephroccisano@hotmail.com>
2018-06-27 00:13:24 +1000

    Buffer issue fixed + cleanup

--- send_data_csv1.py
+++ send_data_csv1.py
@@ -1,31 +1,6 @@
 #! /usr/bin/python
-# Written by Dan Mandle http://dan.mandle.me September 2012
-# License: GPL 2.0
-
-import os
 from gps import *
-from time import *
-import time
-import threading
 from datetime import datetime
-
-# gpsd = None #seting the global variable
-#
-# os.system('clear') #clear the terminal (optional)
-#
-# class GpsPoller(threading.Thread):
-#   def __init__(self):
-#     threading.Thread.__init__(self)
-#     global gpsd #bring it in scope
-#     gpsd = gps(mode=WATCH_ENABLE) #starting the stream of info
-#     self.current_value = None
-#     self.running = True #setting the thread running to true
-#
-#   def run(self):
-#     global gpsd
-#     while gpsp.running:
-#       print 'next'
-#       gpsd.next() #this will continue to loop and grab EACH set of gpsd info to clear the $
 
 if __name__ == '__main__':
   gpsd = gps(mode=WATCH_ENABLE)
@@ -50,7 +25,4 @@ if __name__ == '__main__':
               old_buffer_value = buffer[-1]
 
   except (KeyboardInterrupt, SystemExit): #when you press ctrl+c
-    print "\nKilling Thread..."
-    # gpsp.running = False
-    # gpsp.join() # wait for the thread to finish what it's doing
-  print "Done.\nExiting."
+    print "Goodbye"
