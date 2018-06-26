53825626ce2b8262c48ec6baf5b1f01f2b1ca2f2
Joseph <josephroccisano@hotmail.com>
2018-06-26 20:22:46 +1000

    Add dos2unix to begin file

--- begin.sh
+++ begin.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
+echo "Downloading Required Files"
+sudo apt-get install dos2unix
+sudo apt-get install gpsd gpsd-clients python-gps
+
 echo "GPS Initialising"
 sudo killall gpsd
 sudo gpsd /dev/ttyS0 -F /var/run/gpsd.sock
 
 echo "Activate Python Script"
+dos2unix send_data.py
 python send_data.py
