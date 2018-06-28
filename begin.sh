1dcbfbd49435e3ad34bf7212d925092584ad00a0
Joseph Roccisano <josephroccisano@hotmail.com>
2018-06-28 13:54:36 +0000

    Fixed bash file

--- begin.sh
+++ begin.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
-#echo "Downloading Required Files"
-#sudo apt-get install dos2unix
-#sudo apt-get install gpsd gpsd-clients python-gps
-
-echo "GPS Initialising"
+echo "Initialising GPSD"
 sudo killall gpsd
 sudo gpsd /dev/ttyS0 -F /var/run/gpsd.sock
-
-#echo "Activate Python Script"
-#dos2unix send_data.py
-#python send_data.py
+echo "Done! Deploy your application"
